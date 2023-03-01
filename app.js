9dd7908640d23bd078b956af293401ccd6dfb7b3
alindaByamukama <alindabyamukama@gmail.com>
2023-03-01 18:34:46 +0300

    configure eslint and mocha - add eslintrc json file

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ const passport = require('passport');
 // Express session
 const expressSession = require('express-session')({
   secret: 'hermit#9',
-  resave: false,                                        // don't save session if unmodified
-  saveUninitialized: false,                             // don't create session until something is stored
-})
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something is stored
+});
 
 // Import the User Model or Schema
 const ClientRegister = require('./models/entries');
@@ -26,21 +26,22 @@ const { exists } = require('./models/entries');
 const app = express();
 
 // SetUp Database Connections
-mongoose.connect(config.database, { useNewUrlParser: true});
+mongoose.connect(config.database, { useNewUrlParser: true });
 const db = mongoose.connection;
 
 // Check connection
-db.once('open', function(){
+db.once('open', () => {
   console.log('Connected to MongoDB now');
 });
 
 // Check for db errors
-db.on('error', function(err){
+db.on('error', (err) => {
   console.error(err);
 });
 
 // CONFIGURATIONS-------------------**** section in Anatomy of an Express Server
 app.engine('pug', require('pug').__express);
+
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -54,18 +55,18 @@ app.use('/', calcroute);
 
 app.get('/', (req, res) => {
   res.send('Welcome home. This is Cohort 12, mbu!');
-})
+});
 
 app.get('/index', (req, res) => {
   res.render('index');
-})
+});
 
 app.get('*', (req, res) => {
-  res.send("404! Sorry the page you're looking for does not exists.")
-})
+  res.send("404! Sorry the page you're looking for does not exists.");
+});
 
 // BOOTSTRAPPING SERVER--------------***section in Anatomy of an Express Server
-// const PORT = 4000;
-// const HOST = '0.0.0.0';
+const PORT = 4000;
+const HOST = '0.0.0.0';
 
 app.listen(PORT, HOST, () => console.log(`Elders are listening on ${HOST}:${PORT}`));
