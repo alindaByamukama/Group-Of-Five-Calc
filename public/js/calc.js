1ab2af169c862908ed890510fd05d5f5fb27a032
alindaByamukama <alindabyamukama@gmail.com>
2023-03-02 05:53:36 +0300

    add changes to calc file - revert back to original state before testing - remove export

--- public/js/calc.js
+++ public/js/calc.js
@@ -1,59 +1,55 @@
-let num1 = document.getElementById('num1');
-let num2 = document.getElementById('num2');
-const result = document.getElementById('result');
-
 function addNumbers() {
   // Get the values of the input elements
-  num1 = parseInt(num1.value, 10);
-  num2 = parseInt(num2.value, 10);
+  const num1 = parseInt(document.getElementById('num1').value, 10);
+  const num2 = parseInt(document.getElementById('num2').value, 10);
 
   // Add the two numbers together
   const sum = num1 + num2;
 
   // Display the result in the paragraph element
-  result.textContent = `Result = ${sum}`;
+  document.getElementById('result').textContent = `Result = ${sum}`;
+};
+
+function subtractNumbers() {
+  // Get the values of the input elements
+  const num1 = parseInt(document.getElementById('num1').value, 10);
+  const num2 = parseInt(document.getElementById('num2').value, 10);
+
+  // Add the two numbers together
+  const difference = num1 - num2;
+
+  // Display the result in the paragraph element
+  document.getElementById('result').textContent = `Result = ${difference}`;
 }
 
-// function subtractNumbers() {
-//   // Get the values of the input elements
-//   const num1 = parseInt(document.getElementById('num1').value, 10);
-//   const num2 = parseInt(document.getElementById('num2').value, 10);
+function multiplyNumbers() {
+  // Get the values of the input elements
+  const num1 = parseInt(document.getElementById('num1').value, 10);
+  const num2 = parseInt(document.getElementById('num2').value, 10);
 
-//   // Add the two numbers together
-//   const difference = num1 - num2;
+  // Add the two numbers together
+  const product = num1 * num2;
 
-//   // Display the result in the paragraph element
-//   document.getElementById('result').textContent = `Result = ${difference}`;
-// }
+  // Display the result in the paragraph element
+  document.getElementById('result').textContent = `Result = ${product}`;
+}
 
-// function multiplyNumbers() {
-//   // Get the values of the input elements
-//   const num1 = parseInt(document.getElementById('num1').value, 10);
-//   const num2 = parseInt(document.getElementById('num2').value, 10);
+function divideNumbers() {
+  // Get the values of the input elements
+  const num1 = parseInt(document.getElementById('num1').value, 10);
+  const num2 = parseInt(document.getElementById('num2').value, 10);
 
-//   // Add the two numbers together
-//   const product = num1 * num2;
+  // Add the two numbers together
+  const quotient = num1 / num2;
 
-//   // Display the result in the paragraph element
-//   document.getElementById('result').textContent = `Result = ${product}`;
-// }
+  // Display the result in the paragraph element
+  document.getElementById('result').textContent = `Result = ${quotient.toFixed(2)}`;
+}
 
-// function divideNumbers() {
-//   // Get the values of the input elements
-//   const num1 = parseInt(document.getElementById('num1').value, 10);
-//   const num2 = parseInt(document.getElementById('num2').value, 10);
+function clearScreen() {
+  document.getElementById('result').textContent = '';
+  document.getElementById('num1').value = '';
+  document.getElementById('num2').value = '';
+}
 
-//   // Add the two numbers together
-//   const quotient = num1 / num2;
-
-//   // Display the result in the paragraph element
-//   document.getElementById('result').textContent = `Result = ${quotient.toFixed(2)}`;
-// }
-
-// function clearScreen() {
-//   document.getElementById('result').textContent = '';
-//   document.getElementById('num1').value = '';
-//   document.getElementById('num2').value = '';
-// }
-
-module.exports.addNumbers = addNumbers;
+// module.exports.addNumbers = addNumbers;
