17ff2fb2d324114c46621ee6c00cb41659b1ed09
alindaByamukama <alindabyamukama@gmail.com>
2023-03-02 04:53:43 +0300

    fix typo add require to addNumbers import in test file - mocha tests return "TypeError: addNumbers is not a function"

--- test/test-add.js
+++ test/test-add.js
@@ -1,9 +1,12 @@
-const addNumbers = 'Group-Of-Five-Calc/public/js/calc';
-
 const { assert } = require('chai');
+const addNumbers = require('../public/js/calc');
+
 
 describe('Test addNumbers function', () => {
   it('failing test', () => {
     assert.equal(addNumbers(1, 1), 1);
+  });
+  it('passing test', () => {
+    assert.equal(addNumbers(1, 1), 2);
   });
 });
