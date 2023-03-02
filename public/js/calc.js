5db4d051bc1f4e6c5efb1b66a29ab36b3079972b
alindaByamukama <alindabyamukama@gmail.com>
2023-03-02 05:05:23 +0300

    fix typo add require to addNumbers import in test file - mocha tests return "ReferenceError: document is not defined"

--- public/js/calc.js
+++ public/js/calc.js
@@ -1,55 +1,59 @@
+let num1 = document.getElementById('num1');
+let num2 = document.getElementById('num2');
+const result = document.getElementById('result');
+
 function addNumbers() {
   // Get the values of the input elements
-  const num1 = parseInt(document.getElementById('num1').value, 10);
-  const num2 = parseInt(document.getElementById('num2').value, 10);
+  num1 = parseInt(num1.value, 10);
+  num2 = parseInt(num2.value, 10);
 
   // Add the two numbers together
   const sum = num1 + num2;
 
   // Display the result in the paragraph element
-  document.getElementById('result').textContent = `Result = ${sum}`;
-};
-
-function subtractNumbers() {
-  // Get the values of the input elements
-  const num1 = parseInt(document.getElementById('num1').value, 10);
-  const num2 = parseInt(document.getElementById('num2').value, 10);
-
-  // Add the two numbers together
-  const difference = num1 - num2;
-
-  // Display the result in the paragraph element
-  document.getElementById('result').textContent = `Result = ${difference}`;
+  result.textContent = `Result = ${sum}`;
 }
 
-function multiplyNumbers() {
-  // Get the values of the input elements
-  const num1 = parseInt(document.getElementById('num1').value, 10);
-  const num2 = parseInt(document.getElementById('num2').value, 10);
+// function subtractNumbers() {
+//   // Get the values of the input elements
+//   const num1 = parseInt(document.getElementById('num1').value, 10);
+//   const num2 = parseInt(document.getElementById('num2').value, 10);
 
-  // Add the two numbers together
-  const product = num1 * num2;
+//   // Add the two numbers together
+//   const difference = num1 - num2;
 
-  // Display the result in the paragraph element
-  document.getElementById('result').textContent = `Result = ${product}`;
-}
+//   // Display the result in the paragraph element
+//   document.getElementById('result').textContent = `Result = ${difference}`;
+// }
 
-function divideNumbers() {
-  // Get the values of the input elements
-  const num1 = parseInt(document.getElementById('num1').value, 10);
-  const num2 = parseInt(document.getElementById('num2').value, 10);
+// function multiplyNumbers() {
+//   // Get the values of the input elements
+//   const num1 = parseInt(document.getElementById('num1').value, 10);
+//   const num2 = parseInt(document.getElementById('num2').value, 10);
 
-  // Add the two numbers together
-  const quotient = num1 / num2;
+//   // Add the two numbers together
+//   const product = num1 * num2;
 
-  // Display the result in the paragraph element
-  document.getElementById('result').textContent = `Result = ${quotient.toFixed(2)}`;
-}
+//   // Display the result in the paragraph element
+//   document.getElementById('result').textContent = `Result = ${product}`;
+// }
 
-function clearScreen() {
-  document.getElementById('result').textContent = '';
-  document.getElementById('num1').value = '';
-  document.getElementById('num2').value = '';
-}
+// function divideNumbers() {
+//   // Get the values of the input elements
+//   const num1 = parseInt(document.getElementById('num1').value, 10);
+//   const num2 = parseInt(document.getElementById('num2').value, 10);
+
+//   // Add the two numbers together
+//   const quotient = num1 / num2;
+
+//   // Display the result in the paragraph element
+//   document.getElementById('result').textContent = `Result = ${quotient.toFixed(2)}`;
+// }
+
+// function clearScreen() {
+//   document.getElementById('result').textContent = '';
+//   document.getElementById('num1').value = '';
+//   document.getElementById('num2').value = '';
+// }
 
 module.exports.addNumbers = addNumbers;
--- test/test-add.js
+++ test/test-add.js
@@ -1,7 +1,6 @@
 const { assert } = require('chai');
 const addNumbers = require('../public/js/calc');
 
-
 describe('Test addNumbers function', () => {
   it('failing test', () => {
     assert.equal(addNumbers(1, 1), 1);
